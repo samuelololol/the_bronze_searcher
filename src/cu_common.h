18b0916a93338fba0d1d525c147d721abd270f46
samuelololol <samuelololol@gmail.com>
2017-04-06 13:36:11 +0800

    combine the typedef and struct definition

--- src/cu_common.h
+++ src/cu_common.h
@@ -2,12 +2,11 @@
 #define __CU_COMMON_H__
 #include <stdbool.h>
 
-typedef struct _search_result Search_result;
-struct _search_result {
+typedef struct _search_result {
     char *path;
     unsigned int line_no;
     /* unsigned int column; */
-};
+} Search_result;
 
 bool is_text_file(const char *path);
 bool is_folder(const char *path);
